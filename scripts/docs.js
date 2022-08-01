184fc2a2419b319288bb5abbe54f9f1279f5160f
esdmr <esdmr0@gmail.com>
2022-08-01 18:12:21 +0430

    Convert repo to a monorepo

--- scripts/docs.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import { execaCommand } from 'execa';
-
-await execaCommand('pnpm exec typedoc src/index.ts', {
-	stdio: 'inherit',
-});
---
+++ scripts/upload-coverage.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import { existsSync } from 'node:fs';
+import {readdir} from 'node:fs/promises';
+import path from 'node:path';
+import process from 'node:process';
+import {execa} from 'execa';
+
+const packagesDir = 'packages';
+
+for (const packageName of await readdir(packagesDir)) {
+	const lcov = path.join(packagesDir, packageName, 'coverage', 'lcov.info');
+
+	if (!existsSync(lcov)) {
+		console.log(`Skipped uploading ${packageName}. lcov file not found.`);
+		continue;
+	}
+
+	console.log(`::group::Uploading ${packageName}…`);
+
+	await execa('codecov', [
+		...process.argv.slice(2),
+		'-f', lcov,
+		'-F', packageName,
+	], {
+		stdio: 'inherit',
+	});
+
+	console.log('::endgroup::');
+}
