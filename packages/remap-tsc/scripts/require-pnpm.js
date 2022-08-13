b97d3cdf36166abbbbf14867eae976af7a9da7a7
esdmr <esdmr0@gmail.com>
2022-08-14 01:02:34 +0430

    Remove unneeded script

--- packages/remap-tsc/scripts/require-pnpm.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import process from 'node:process';
-
-if (!process.env.npm_config_user_agent?.startsWith('pnpm/')) {
-	console.error('This script must be run by pnpm.');
-	process.exit(1);
-}
