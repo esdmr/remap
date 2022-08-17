983a7fcad84fbbdb1ef643c226b39c1d29b41bab
esdmr <esdmr0@gmail.com>
2022-08-17 15:47:56 +0430

    Setup nx

--- packages/remap-tsc/scripts/build.js
+++ packages/remap-tsc/scripts/build.js
@@ -1,85 +1,44 @@
 #!/usr/bin/env node
-import process from 'node:process';
 import { fileURLToPath } from 'node:url';
 import assert from 'node:assert';
 import { build } from 'esbuild';
-import { execa } from 'execa';
+import { execaCommand } from 'execa';
 import isPnpm from '@esdmr/scripts/is-pnpm';
 
 assert(isPnpm, 'This script must be run by pnpm');
 
-if (process.argv.includes('--help') || process.argv.includes('-h')) {
-	console.log(`\
-Usage: pnpm build [options]
-
-Options:
---watch  Watch for changes
---dev    Disable identifier minification`);
-	process.exit();
-}
-
-const shouldWatch = process.argv.includes('--watch');
-const isDev = shouldWatch || process.argv.includes('--dev');
-
-if (isDev) {
-	console.info('This is a development build.');
-}
+/** @param {string} path */
+const resolvePath = (path) => fileURLToPath(new URL(path, import.meta.url));
 
 /** @type {import('esbuild').BuildOptions} */
 const buildOptions = {
 	absWorkingDir: resolvePath('..'),
 	platform: 'node',
-	watch: shouldWatch,
 	bundle: true,
-	minify: !isDev,
 	keepNames: true,
-	minifySyntax: isDev,
-	minifyWhitespace: isDev,
+	minifySyntax: true,
+	minifyWhitespace: true,
 	sourcemap: true,
 	sourcesContent: false,
 	external: ['node:path', 'typescript'],
 	target: 'node14',
 };
 
-const esmResult = await build({
+await build({
 	...buildOptions,
 	entryPoints: ['src/index.ts'],
 	outfile: 'build/index.js',
 	format: 'esm',
 });
 
-const cjsResult = await build({
+await build({
 	...buildOptions,
 	entryPoints: ['src/index.ts'],
 	outfile: 'build/index.cjs',
 	format: 'cjs',
 });
 
-const options = ['-b'];
-
-if (shouldWatch) {
-	options.push('-w', '--preserveWatchOutput');
-}
-
-const tscResult = execa('tsc', options, {
+await execaCommand('tsc -b', {
 	stdio: 'inherit',
+	cwd: resolvePath('..'),
 });
-
-if (shouldWatch) {
-	console.log('Watching for changes…');
-
-	process.once('SIGINT', () => {
-		console.log('\rStopping…');
-		esmResult.stop?.();
-		cjsResult.stop?.();
-		tscResult?.kill();
-		process.exit(0);
-	});
-} else {
-	await tscResult;
-}
-
-/** @param {string} path */
-function resolvePath (path) {
-	return fileURLToPath(new URL(path, import.meta.url));
-}
---
+++ packages/scripts/run.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+import process from 'node:process';
+import assert from 'node:assert';
+import { execa } from 'execa';
+import isPnpm from './is-pnpm.js';
+
+assert(isPnpm, 'This script must be run by pnpm');
+
+const [target, argv] = parseArgv();
+const isPartial = process.env.GITHUB_PR_TO_MAIN !== 'false';
+
+console.log('Running', isPartial ? 'affected' : 'all', 'packages.');
+
+try {
+	if (isPartial) {
+		await execa('nx', [
+			'run-many',
+			'--target=' + target,
+			...argv,
+		], {
+			stdio: 'inherit',
+		});
+	} else {
+		await execa('nx', [
+			'affected',
+			'--base=origin/main',
+			'--head=HEAD',
+			'--target=' + target,
+			...argv,
+		], {
+			stdio: 'inherit',
+		});
+	}
+} catch (error) {
+	process.exitCode = 1;
+
+	if (error instanceof Error) {
+		const code = /** @type {Record<string, any>} */(error).exitCode;
+
+		if (typeof code === 'number') {
+			process.exitCode = code;
+		}
+	}
+
+	process.exit();
+}
+
+/**
+ * @returns {[string, string[]]}
+ */
+function parseArgv () {
+	const argv = process.argv.slice(2);
+	const target = argv.shift();
+
+	if (!target) {
+		console.error('Usage: pnpm exec run <target> [options] [arguments]');
+		process.exit(1);
+	}
+
+	return [
+		target,
+		argv,
+	];
+}
